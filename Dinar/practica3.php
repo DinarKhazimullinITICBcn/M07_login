4004d741bdd7e44e1cb916b70a5ecad35226d3c5
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2023-10-06 17:23:29 +0200

    Fet el arxiu php amb connexio a la base de dades

--- Dinar/practica3.php
+++ Dinar/practica3.php
@@ -1,11 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php 
+    if (isset($_GET['u_id'])) {
+        $u_id = $_GET['u_id'];
+    } else {
+        $u_id  = null;
+    }
+    if (isset($_GET['u_rol'])) {
+        $u_rol = $_GET['u_rol'];
+    } else {
+        $u_rol  = null;
+    }
+    if (isset($_GET['u_name'])) {
+        $u_name = strval($_GET['u_name']);
+    } else {
+        $u_name  = null;
+    }
+    if (isset($_GET['u_surname'])) {
+        $u_surname = strval($_GET['u_surname']);
+    } else {
+        $u_surname  = null;
+    }
+    if (isset($_GET['u_password'])) {
+        $u_password = strval($_GET['u_password']);
+    } else {
+        $u_password  = null;
+    }
+    if (isset($_GET['u_email'])) {
+        $u_email = strval($_GET['u_email']);
+    } else {
+        $u_email  = null;
+    }
+    if (isset($_GET['u_active'])) {
+        $u_active = $_GET['u_active'];
+    } else {
+        $u_active  = null;
+    }
     
-</body>
-</html>
+    //Variables de SQL
+    $db_host = "localhost";
+    $db_nombre = "users";
+    $db_usuario = "root";
+    $db_passwd = "";
+    //Connexio SQL
+    $connexio = mysqli_connect($db_host, $db_usuario, $db_passwd, $db_nombre);
+    $consulta = "INSERT INTO user (id, rol, name, surname, password, email, active) VALUE ('$u_id', '$u_rol', '$u_name', '$u_surname', '$u_password', '$u_email', '$u_active')";
+    $products = mysqli_query($connexio, $consulta)
+?>
