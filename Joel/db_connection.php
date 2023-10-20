a48108608aedaafd8300378b0f35d6857feb2ac5
Joel Ghanem <=>
2023-10-20 09:34:36 +0200

    db connection actualitzada

--- Joel/db_connection.php
+++ Joel/db_connection.php
@@ -1,1 +1,13 @@
-creat
+<?php
+//Dades necessaries per conectar amb el server
+$host = "localhost";
+$nom = "users";
+$usuari = "root";
+$contra = "";
+
+try {
+  $conexio = mysqli_connect($host, $usuari, $contra, $nom);
+} catch (Exception $e) {
+  echo(`No hi ha hagut connexió amb la base, error: $e`)
+}
+?>
