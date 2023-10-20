918ad14ca9d60ce868d426a16540d0996edb7310
Joel Ghanem <=>
2023-10-20 10:10:31 +0200

    userLogin updated

--- Joel/iniciar_sessio/userLogin.php
+++ Joel/iniciar_sessio/userLogin.php
@@ -1,1 +1,11 @@
-creado
+<?php
+include('../db_connection.php');
+$query = "select password from user where email = '2023_joel.ghanem@iticbcn.cat';";
+$dades = mysqli_query($connection, $query);
+$password = (string)$dades;
+echo $password;
+if (isset($_POST['send'])){
+  $pwd = $_POST['pwd'];
+  $mail = $_POST['mail'];
+}
+?>
