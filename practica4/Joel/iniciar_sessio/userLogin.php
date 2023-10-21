a72076522f4258282e6db7c8d2fdd51baf6aea49
Joel Ghanem <=>
2023-10-21 23:15:57 +0200

    userLogin.php correcte

--- practica4/Joel/iniciar_sessio/userLogin.php
+++ practica4/Joel/iniciar_sessio/userLogin.php
@@ -3,29 +3,28 @@
 <?php
 include('../db_connection.php');
 
-if (isset($_POST['send'])) {
-  $mail = $_POST['email'];
-  $pwd = $_POST['password'];
+$mail = $_POST['email'];
+$pwd = $_POST['password'];
 
-  $query = "SELECT * FROM user WHERE email = '$mail'";
-  $result = mysqli_query($connection, $query);
+$query = "SELECT * FROM user WHERE email = '$mail'";
+$result = mysqli_query($connection, $query);
 
-  if (mysqli_num_rows($result) > 0) {
-    $row = mysqli_fetch_assoc($result);
-    $storedPassword = $row['password'];
+if (mysqli_num_rows($result) > 0) {
+  $row = mysqli_fetch_assoc($result);
+  $storedPassword = $row['password'];
 
-    // Verifica la contraseña ingresada con la contraseña almacenada
-    if ($password == $pwd) {
-      // Contraseña correcta, redirige al usuario a usuari.php
-      header('Location: usuari.php?email=' . $row['email']);
-    } else {
-      // Contraseña incorrecta, redirige al usuario a login.php con mensaje de error
-      header('Location: login.php?error=incorrecto');
-    }
+  // Verifica la contraseña ingresada con la contraseña almacenada
+  if ($storedPassword == $pwd) {
+    // Contraseña correcta, redirige al usuario a usuari.php
+    header('Location: ../views/usuari.php?email=' . $row['email']);
+  } else {
+    // Contraseña incorrecta, redirige al usuario a login.php con mensaje de error
+   include("../views/login.html");
+    echo "Password incorrecte";
   }
-} else {
-    include('../views/login.html');
-    echo "Login incorrecte ";
+} else{
+   include("../views/login.html");
+    echo "Email incorrecte";
 }
 ?>
 
