fc07dc03cfa4407e49de246c76150cdf4d7384ae
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2023-10-27 17:27:23 +0200

    Modificat la creacio de usuaris per a que sigui modular

---
+++ Practica_5/Dinar/crear_usuari/index.php
@@ -0,0 +1,38 @@
+<?php
+//incluim la conexio
+include('../db_connection.php');
+//Mirem si la informacio que s'envia esta buida o no.
+if (isset($_POST['send'])){
+    $id = $_POST['id'];
+    $nom = $_POST['name'];
+    $cognom = $_POST['surname'];
+    $rol_user = $_POST['ROL'];
+    $pass = $_POST['passwd'];
+    $email = $_POST['email'];
+    $actiu = $_POST['active'];
+//Si es actiu ho convertim en True y si no en false
+    if ($actiu = 'si'){
+        $actiu = 'True';
+    }else{
+        $actiu = 'False';
+    }
+
+    //Agafem llenguatge de la cookie
+    $llenguatge = $_COOKIE["llenguatge"];
+
+
+    //Fem consulta query
+    $consulta = "INSERT INTO `user`(`ID`, `NAME`, `SURNAME`, `ROL`, `PASSWORD`, `EMAIL`, `ACTIVE`) VALUES ('$id', '$nom', '$cognom', '$rol_user', '$pass', '$email', $actiu)";
+
+    
+    //Introduim les dades de consulta a la taula user
+    $result = mysqli_query($conn, $consulta);
+       
+    if(!$result){
+        die("Query fail!");
+    }
+    
+    header("Location: ../views/" . $llenguatge . "/login.html");
+    
+}
+?>
