350ba3489ec67b02b65b4bbaf1811d00b009941a
Joel Ghanem <=>
2023-10-28 12:31:18 +0200

    Arxiu per crear l'usuari a la base de dades

---
+++ Practica_5/Joel/crear_usuari/index.php
@@ -0,0 +1,37 @@
+
+<?php
+include("../db_connection.php");
+
+if ($conn->connect_error) {
+  die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Verificar todas las variables en un solo isset
+if (isset($_POST['id'], $_POST['rol'], $_POST['name'], $_POST['surname'], $_POST['password'], $_POST['email'])) {
+  $id = $_POST['id'];
+  $rol = $_POST['rol'];
+  $name = $_POST['name'];
+  $surname = $_POST['surname'];
+  $password = $_POST['password'];
+  $email = $_POST['email'];
+
+  $active = isset($_POST['active']) ? 1 : 0;
+
+  $sql = "INSERT INTO user (id, rol, name, surname, password, email, active)
+          VALUES ('$id', '$rol', '$name', '$surname', '$password', '$email', '$active');";
+
+  var_dump($sql);
+
+  if ($connection->query($sql) === TRUE) {
+
+
+    // Redirige al usuario a la página de inicio de sesión
+    header('Location: ../views/login.html');
+  } else {
+  die("");
+  }
+
+$connection->close();
+?>
+
+
