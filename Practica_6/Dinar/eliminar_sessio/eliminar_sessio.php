b7da547ec58897f2389a60789a60c3e5fdb09fbb
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2023-11-03 17:26:36 +0100

    Afegit comentaris

--- Practica_6/Dinar/eliminar_sessio/eliminar_sessio.php
+++ Practica_6/Dinar/eliminar_sessio/eliminar_sessio.php
@@ -1,6 +1,9 @@
 <?php
+//Inicialitzacio de la sessio
 session_start();
+//Borrem les dades de les sessions
 session_unset();
+//Borrem la sessio completament
 session_destroy();
 header('Location: ../views/login.html');
 ?>
